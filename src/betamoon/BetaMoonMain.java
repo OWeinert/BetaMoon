8d209ac70610a167ac91c3bcbb2b6cd1ee8c995a
Piggo41 <mr.piggo123@gmail.com>
2026-01-17 03:30:03 +0100

    Corrected version

--- src/betamoon/BetaMoonMain.java
+++ src/betamoon/BetaMoonMain.java
@@ -22,7 +22,7 @@ import net.minecraft.src.ModLoader;
 import net.minecraft.src.World;
 
 public final class BetaMoonMain {
-    private static final String VERSION = "0.5.1";
+    private static final String VERSION = "0.5.0";
     public static final String LUA_SCRIPTS_DIR = "lua_scripts";
     public static final Logger LOGGER = Logger.getLogger("BetaMoon");
     static {
